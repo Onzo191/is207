5571835b426a5a24916dd2a1fa4a26d39868479c
Hung Tuan Nguyen <hungtuan.onzo@gmail.com>
2023-09-25 20:41:57 +0700

    completed change quantity, remove product features

--- Homework/shopping-cart/scripts/script.js
+++ Homework/shopping-cart/scripts/script.js
@@ -36,18 +36,43 @@ const cart = [
 let discountCode = 'DISCOUNT10';
 let address = '123 Main St, City, Country';
 
-function updateCartDisplay() {
+// Callback
+document.addEventListener('click', function (event) {
+  const classList = event.target.classList;
+  console.log(event.target);
+  if (classList.contains('qty-decrease')) {
+    const productId = event.target.getAttribute('data-id');
+    updateProductQuantity(productId, -1);
+  } else if (classList.contains('qty-increase')) {
+    const productId = event.target.getAttribute('data-id');
+    updateProductQuantity(productId, 1);
+  } else if (classList.contains('remove-button')) {
+    const productId = event.target.getAttribute('data-id');
+    removeProduct(productId);
+  } else if (classList.contains('clear-button')) {
+    clearCart();
+  }
+
+  // Blabla, luoi qua!
+});
+
+// Cart item display
+const updateCartDisplay = () => {
   const cartList = document.getElementById('product-list');
 
   // Clear existing items
   cartList.innerHTML = '';
 
-  cart.forEach((item) => {
-    const { id, imgUrl, name, productTitle, deliveryTime, price, discount, quantity } = item;
-    const discountPrice = price * (1 - discount);
-    const listItem = document.createElement('div');
-    listItem.className = 'product-row';
-    listItem.innerHTML = `
+  if (!cart.length) {
+    cartList.innerHTML = 'Your Cart is Empty!';
+  } else
+    cart.forEach((item) => {
+      const { id, imgUrl, name, productTitle, deliveryTime, price, discount, quantity } = item;
+      const discountPrice = Math.ceil((price * (1 - discount)) / 10000) * 10000;
+
+      const listItem = document.createElement('div');
+      listItem.className = 'product-row';
+      listItem.innerHTML = `
     <div class="col1">
       <div class="product-container">
         <input type="checkbox" />
@@ -68,13 +93,13 @@ function updateCartDisplay() {
     </div>
     <div class="col3">
       <div class="qty-button-group">
-        <span class="qty-decrease"
+        <span class="icon-button qty-decrease" data-id="${id}"
           ><img
             src="./assets/images/decrease.svg"
             alt="decrease"
         /></span>
-        <input type="tel" class="qty-input" value="${quantity}" />
-        <span class="qty-increase"
+        <input type="tel" class="qty-input" value="${quantity}" min="1" data-id="${id}"/>
+        <span class="icon-button qty-increase" data-id="${id}"
           ><img
             src="./assets/images/increase.svg"
             alt="increase"
@@ -85,16 +110,74 @@ function updateCartDisplay() {
       discountPrice * quantity
     ).toLocaleString()}₫</span></div>
     <div class="col5">
-      <button class="icon-button">
+      <span class="icon-button remove-button" data-id="${id}">
         <img
           src="./assets/images/trash.svg"
           alt="deleted"
         />
-      </button>
+      </span>
     </div>
     `;
-    cartList.appendChild(listItem);
-  });
-}
+      cartList.appendChild(listItem);
+    });
 
+  updateFinalPrice();
+};
+
+// Update Total Price
+const updateFinalPrice = () => {
+  const cartTotal = calculateCartTotal();
+
+  // gonna have discount or sumthin' change the final price
+  const totalPriceElement = document.getElementById('total-price');
+  const totalPrice = cartTotal;
+
+  // freakin final price
+  const finalPriceElement = document.getElementById('total-price-final');
+  const finalPrice = cartTotal;
+  totalPriceElement.textContent = `${totalPrice.toLocaleString()}₫`;
+  finalPriceElement.textContent = `${finalPrice.toLocaleString()}₫`;
+};
+
+const calculateCartTotal = () => {
+  let total = 0;
+  cart.forEach((item) => {
+    const { price, discount, quantity } = item;
+    const discountPrice = Math.ceil((price * (1 - discount)) / 10000) * 10000;
+    total += discountPrice * quantity;
+  });
+  return total;
+};
+
+// Update product quantity
+const updateProductQuantity = (productId, change) => {
+  const product = cart.find((item) => item.id === productId);
+  if (product) {
+    product.quantity += change;
+    if (product.quantity < 1) {
+      alert('Product Removed!');
+      // product.quantity = 1;
+      removeProduct(productId);
+    }
+
+    updateCartDisplay();
+  }
+};
+
+// Remove product
+const removeProduct = (productId) => {
+  const productIndex = cart.findIndex((item) => item.id === productId);
+  if (productIndex !== -1) {
+    cart.splice(productIndex, 1);
+    updateCartDisplay();
+  }
+};
+
+// Clear cart
+const clearCart = () => {
+  cart.splice(0, cart.length);
+  updateCartDisplay();
+};
+
+// mount #1
 updateCartDisplay();
